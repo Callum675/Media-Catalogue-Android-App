380632d101791d2c11bc26b3baa30862256d97e7
Callum675 <callummcintosh6675@gmail.com>
2021-11-09 17:43:15 +0000

    updates

---
+++ MovieandSongCatalogue/app/src/main/java/com/example/movieandsongcatalogue/data/Search.java
@@ -0,0 +1,15 @@
+package com.example.movieandsongcatalogue.data;
+
+public class Search {
+
+    private String search;
+
+    //getter
+    public String getSearch() {
+        return search;
+    }
+
+    //setter
+    public void setSearch(String search) { this.search = search; }
+
+}
